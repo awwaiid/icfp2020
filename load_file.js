f180608cc3dac41eed6a7bd534fb8c97587f8063
Brock Wilcox <awwaiid@thelackthereof.org>
2020-07-17 15:09:26 -0400

    add initial file loading

---
+++ load_file.js
@@ -0,0 +1,42 @@
+// MARIO INSERT ANSWER HERE
+
+// (1) read file, probably one line at a time
+// (2) for each line, split it into the left (memory address) of the = and the right (program)
+// (3) for the stuff on the right turn it into an array
+// (4) we'll then use the interpreter that we didn't write yet to interpret each line and store the result in an array based on the "memory address"
+// (5) ...
+// (6) profit?
+// (7) glory!
+
+// https://attacomsian.com/blog/reading-a-file-line-by-line-in-nodejs
+
+
+
+// other stuff
+const fs = require('fs');
+
+//import fs from "fs";
+
+
+try {
+    
+    const data = fs.readFileSync('galaxy.txt', 'UTF-8');
+
+    const lines = data.split(/\r?\n/);
+
+    lines.forEach((fileLine) => {
+        console.log(fileLine);
+
+        
+
+    });
+
+
+} catch (err) {
+
+    console.error(err);
+    
+}
+
+
+
