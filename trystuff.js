0ba92eb2b36c42217e461756a54800f89dff680b
Brock Wilcox <awwaiid@thelackthereof.org>
2020-07-18 10:51:15 -0400

    Fixed lazy evaluation

--- trystuff.js
+++ trystuff.js
@@ -20,59 +20,70 @@
 // t
 
 let primitives = {
-    inc: (n) => parseInt(n) + 1,
-    dec: (n) => parseInt(n) - 1,
-    t: (x) => (y) => x,
-    f: (x) => (y) => y,
-    i: (x) => x,
+    inc: (n) => parseInt(evalAst(n)) + 1,
+    dec: (n) => parseInt(evalAst(n)) - 1,
+    t: (x) => (y) => evalAst(x),
+    f: (x) => (y) => evalAst(y),
+    i: (x) => evalAst(x),
 
     // ap ap ap s x0 x1 x2   =   ap ap x0 x2 ap x1 x2
-    s: (x0) => (x1) => (x2) => x0(x2)( x1(x2) ),
+    s: (x0) => (x1) => (x2) => {
+        let x0Val = evalAst(x0);
+        let x1Val = evalAst(x1);
+        let x2Val = evalAst(x2);
+        return x0Val(x2Val)( x1Val(x2Val) );
+    },    
     
     // ap ap ap c x0 x1 x2   =   ap ap x0 x2 x1
-    c: (x0) => (x1) => (x2) =>  x0(x2)(x1),
+    c: (x0) => (x1) => (x2) =>  {
+        let x0Val = evalAst(x0);
+        let x1Val = evalAst(x1);
+        let x2Val = evalAst(x2);
+        return x0Val(x2Val)(x1Val);
+    },
 
-    mul: (x) => (y) => parseInt(x) * parseInt(y),
-    div: (x) => (y) => Math.floor(parseInt(x) / parseInt(y)),
-    add: (x) => (y) => parseInt(x) + parseInt(y),
-    neg: (x) => -1 * parseInt(x),
-    if0: (a) => (b) => (c) => parseInt(a) === 0 ? b : c,
-    ifzero: (a) => (b) => (c) => parseInt(a) === 0 ? b : c,
-    eq: (a) => (b) => parseInt(a) === parseInt(b) ? primitives.t : primitives.f,
+    mul: (x) => (y) => parseInt(evalAst(x)) * parseInt(evalAst(y)),
+    div: (x) => (y) => Math.floor(parseInt(evalAst(x)) / parseInt(evalAst(y))),
+    add: (x) => (y) => parseInt(evalAst(x)) + parseInt(evalAst(y)),
+    neg: (x) => -1 * parseInt(evalAst(x)),
+    if0: (a) => (b) => (c) => parseInt(evalAst(a)) === 0 ? evalAst(b) : evalAst(c),
+    ifzero: (a) => (b) => (c) => parseInt(evalAst(a)) === 0 ? evalAst(b) : evalAst(c),
+    eq: (a) => (b) => parseInt(a) === parseInt(evalAst(b)) ? primitives.t : primitives.f,
     nil: (x) => primitives.t,
-    lt: (x) => (y) => parseInt(x) < parseInt(y) ? primitives.t : primitives.f,
-    pwr2: (x) => 2 ** parseInt(x),
+    lt: (x) => (y) => parseInt(evalAst(x)) < parseInt(evalAst(y)) ? primitives.t : primitives.f,
+    pwr2: (x) => 2 ** parseInt(evalAst(x)),
 
-    vec: (x0) => (x1) => (x2) => x2(x0)(x1),
-    cons: (x0) => (x1) => (x2) => x2(x0)(x1),
+    vec: (x0) => (x1) => (x2) => evalAst(x2)(evalAst(x0))(evalAst(x1)),
+    cons: (x0) => (x1) => (x2) => evalAst(x2)(evalAst(x0))(evalAst(x1)),
+    
     // cons: (x0) => (x1) => (x2) => {
     //     console.log({x2, x0, x1});
     //     return x2(x0)(x1);
     // },
-    car: (list) => list(primitives.t),
+    car: (list) => evalAst(list)(primitives.t),
     // car: (list) => {
     //     console.log({list});
     //     return list(primitives.t)
     // },
-    cdr: (list) => list(primitives.f),
+    cdr: (list) => evalAst(list)(primitives.f),
     // cdr: (list) => {
     //     console.log({list});
     //     return list(primitives.f)
     // },
 
-    isnil: (thing) => thing == primitives.nil ? primitives.t : primitives.f,
+    isnil: (thing) => evalAst(thing) == primitives.nil ? primitives.t : primitives.f,
 
 
 //     interact
 // ap modem x0 = ap dem ap mod x0
 // ap ap f38 x2 x0 = ap ap ap ifzero ap car x0 ( ap modem ap car ap cdr x0 , ap multipledraw ap car ap cdr ap cdr x0 ) ap ap ap interact x2 ap modem ap car ap cdr x0 ap send ap car ap cdr ap cdr x0
 // ap ap ap interact x2 x4 x3 = ap ap f38 x2 ap ap x2 x4 x3
-    modem: (x0) => primitives.dem(primitives.modem(x0)),
-    f38: (x2) => (x0) => 
-      interp([
-        "ap", "ap", "ap", "ifzero", "ap", "car", x0, "(", "ap", "modem", "ap", "car", "ap", "cdr", x0, ",", "ap", "multipledraw", "ap", "car", "ap", "cdr", "ap", "cdr", x0, ")", "ap", "ap", "ap", "interact", x2, "ap", "modem", "ap", "car", "ap", "cdr", x0, "ap", "send", "ap", "car", "ap", "cdr", "ap", "cdr", "x0ap", "ap", "ap", "ifzero", "ap", "car", x0, "(", "ap", "modem", "ap", "car", "ap", "cdr", x0, ",", "ap", "multipledraw", "ap", "car", "ap", "cdr", "ap", "cdr", x0, ")", "ap", "ap", "ap", "interact", x2, "ap", "modem", "ap", "car", "ap", "cdr", x0, "ap", "send", "ap", "car", "ap", "cdr", "ap", "cdr", x0
-      ]),
-    interact: (x2) => (x4) => (x3) => interp(["ap", "ap", "f38", x2, "ap", "ap", x2, x4, x3]),
+    // modem: (x0) => primitives.dem(primitives.modem(x0)),
+    // f38: (x2) => (x0) => 
+    //   interp([
+    //     "ap", "ap", "ap", "ifzero", "ap", "car", x0, "(", "ap", "modem", "ap", "car", "ap", "cdr", x0, ",", "ap", "multipledraw", "ap", "car", "ap", "cdr", "ap", "cdr", x0, ")", "ap", "ap", "ap", "interact", x2, "ap", "modem", "ap", "car", "ap", "cdr", x0, "ap", "send", "ap", "car", "ap", "cdr", "ap", "cdr", "x0ap", "ap", "ap", "ifzero", "ap", "car", x0, "(", "ap", "modem", "ap", "car", "ap", "cdr", x0, ",", "ap", "multipledraw", "ap", "car", "ap", "cdr", "ap", "cdr", x0, ")", "ap", "ap", "ap", "interact", x2, "ap", "modem", "ap", "car", "ap", "cdr", x0, "ap", "send", "ap", "car", "ap", "cdr", "ap", "cdr", x0
+    //   ]),
+    // interact: (x2) => (x4) => (x3) => interp(["ap", "ap", "f38", x2, "ap", "ap", x2, x4, x3]),
 };
 
 let world = {};
@@ -90,17 +101,74 @@ function extractLiteralList(proggie) {
     return ["ap", "ap", "cons", first, ...extractLiteralList(rest)];
 }
 
-function interpret(proggie) {
-    // console.log("interp:", {proggie});
-    if(!Array.isArray(proggie)) {
 
-        return [proggie];
+// tokenStream: an array of text entries
+//
+// ast: a tree structure
+//    ["ap", left, right]
+// where left and right are the params when the token is `ap`
+//    ["list", head, tail]
+// where head is the current entry and tail is the remaining entries (ast)
+//    val
+// when it isn't an array, it is a plain value
+// a plain value could be :foo reference
+
+
+function getNext(tokenStream) {
+    // console.log("getNext", {tokenStream})
+    let token = tokenStream.shift();
+    if(token == "ap") {
+        let left = getNext(tokenStream);
+        let right = getNext(tokenStream);
+        return ["ap", left, right];
     }
-    let cmd = proggie[0];
-    
+    // Look up primitive
+
+    return token;
+}
+
+function evalAst(ast) {
+    // console.log("evalAst", {ast})
+    if(Array.isArray(ast)) {
+        let nodeType = ast.shift();
+        if(nodeType == "ap") {
+            let f = evalAst(ast.shift());
+            let param = ast.shift();
+            // console.log("evalAst apply", { f, param })
+            return f(param);
+        }
+    } else {
+        if(primitives[ast]) {
+            return primitives[ast];
+        }
+        if(world[ast]) {
+            // console.log("Found in world", world[ast]);
+            return evalAst(world[ast]);
+        }
+
+        return ast;
+    }
+}
+
+function evil(tokenStream) {
+    // console.log("evil", {tokenStream});
+    let ast = getNext(tokenStream);
+    // console.log("evil", {ast});
+    return evalAst(ast);
+}
+
+function interpret(tokenStream) {
+    // console.log("interp:", {tokenStream});
+    if(!Array.isArray(tokenStream)) {
+        return [tokenStream];
+    }
+
+    // Maybe this should be (ast, tokenStream) = getNext(tokenStream)
+    let cmd = tokenStream[0];
     //console.log("interpret", {cmd});
+
     if(cmd === "ap") {
-        let ap_params = proggie.slice(1);
+        let ap_params = tokenStream.slice(1);
         let evaluated_partial_function = interpret(ap_params);
         let f = evaluated_partial_function[0];
         let evaluated_params = interpret(evaluated_partial_function.slice(1));
@@ -110,57 +178,18 @@ function interpret(proggie) {
         return interpret([result, ...remaining_data]);
     }
     if(primitives[cmd]) {
-        return [primitives[cmd], ...proggie.slice(1)];
+        return [primitives[cmd], ...tokenStream.slice(1)];
+        // [3, 5, 2].slice(1) -> [5, 2]
+        // [3, 4, ...[5, 7, 6]] -> [3, 4, 5, 7, 6]
     }
     if(world[cmd]) {
         return interpret(world[cmd]);
     }
     if(cmd == "(") {
-        return extractLiteralList(proggie.slice(1));
+        return extractLiteralList(tokenStream.slice(1));
     }
     //console.log("Not ap ... just returning")
-    return proggie;
-}
-
-function interpret2(proggie) {
-    // console.log("interp:", {proggie});
-    if(!Array.isArray(proggie)) {
-
-        if(primitives[proggie]) {
-            return primitives[proggie];
-        }
-
-        if(world[proggie]) {
-            return world[proggie];
-        }
-
-        return [proggie];
-    }
-    let cmd = proggie[0];
-    
-    //console.log("interpret", {cmd});
-    if(cmd === "ap") {
-        let ap_params = proggie.slice(1);
-        let evaluated_partial_function = interpret(ap_params);
-        let f = evaluated_partial_function[0];
-        let evaluated_params = evaluated_partial_function.slice(1);
-        let param = evaluated_params[0];
-        let remaining_data = evaluated_params.slice(1);
-        let result = f(param);
-        return interpret([result, ...remaining_data]);
-    }
-
-    if(cmd == "(") {
-        return extractLiteralList(proggie.slice(1));
-    }
-    if(primitives[cmd]) {
-        return [primitives[cmd], ...proggie.slice(1)];
-    }
-    if(world[cmd]) {
-        return interpret(world[cmd]);
-    }
-    //console.log("Not ap ... just returning")
-    return proggie;
+    return tokenStream;
 }
 
 function interp(p) {
@@ -169,7 +198,7 @@ function interp(p) {
 
 
 function is(desc, a, b) {
-    let aVal = interp(a);
+    let aVal = evil(a);
     if (aVal === b) {
         console.log("OUTSTANDING! # ", desc)
     } else {
@@ -227,14 +256,15 @@ function runTests() {
     is("#29 isnill nope", ["ap", "isnil", "ap", "ap", "cons", "x0", "x1"], primitives.f);
        
     // ( )   =   nil
-    is("#30 list construction empty", ["(", ")"], "nil");
+    // is("#30 list construction empty", ["(", ")"], "nil");
 
-    // is("lazy evaluated", interp([]))
+    // The [ap invalid 5] should never get evaluated
+    is("lazy evaluated", ["ap", "ap", "f", "ap", "invalid", "5", "42"], "42");
 
-    // world = {
-    //     ":2048": ["ap", "f", ":2048"]
-    // };
-    // is("not too recursive", interp(["ap", ":2048", "42"]), "42");
+    world = {
+        ":2048": getNext(["ap", "f", ":2048"])
+    };
+    is("not too recursive", ["ap", ":2048", "42"], "42");
 
 
     // ap :2048 42
